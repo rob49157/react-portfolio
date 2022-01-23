40cbcba51cd5d7255ef05871e02d95ffe6740ee7
rob49157 <robertopaipo@hotmail.com>
2022-01-22 22:58:39 -0500

    header

--- root/src/App.js
+++ root/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Thermostat from './components/Thermostat';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
 
 function App() {
   return <Thermostat />;
---
+++ root/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import react from 'react'
+function navbar(){
+    return(
+        <div className='card text-center'>  
+        <div className='card-header bg-warning text-white'> Roberto's React Portfolio</div>
+            
+        </div>
+    )
+
+    
+
+}
+
+
+export default navbar
