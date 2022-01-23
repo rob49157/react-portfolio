f5fabc40fb263920806ee135546fdfbb7429567a
rob49157 <robertopaipo@hotmail.com>
2022-01-23 12:56:09 -0500

    projectpage

--- root/src/App.js
+++ root/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
-import Navbar from './components/Navbar';
+import Projects from './components/Projects';
 import Footer from './components/Footer'
 
 function App() {
   return <div><Header />
-              <Navbar/>
+              <Projects/>
               
         </div>
         
--- root/src/components/Header.js
+++ root/src/components/Header.js
@@ -15,6 +15,9 @@ function Header() {
   return (
     <header style={styles.headerStyle} className="header">
       <h1 style={styles.headingStyle}>Roberto's React Portfolio</h1>
+      <h2> Projects
+      <a href="https://CChampness.github.io/Bike-Adventure-Planning">  🐦Bike-adventure</a>
+      </h2>
     </header>
   );
 }
