f745ecdbb44f5c01b40e1d9cae0edb56cd95b85c
rob49157 <robertopaipo@hotmail.com>
2022-02-24 20:55:40 -0500

    aboutmepg

--- root/src/components/About.js
+++ root/src/components/About.js
@@ -8,7 +8,7 @@ import '../Styles/Header.css'
 import Header from './Header'
 
 function About(){
-    const [aboutSelected,setAboutselected]= useState(" I'm a 32 yr. old regular guy who got interested in technology. About a year ago, I began dabbling with JAVA, followed by some PYTHON. It got to a point where I wasn't disciplined enough with my studies and needed some structure. That's where the GA Tech bootcamp came in. It offered me the structure that I needed to take the final step towards learning coding. I plan to finish the camp in February, and look for work in the field soon thereafter.")
+    const [aboutSelected,setAboutselected]= useState(" I'm a 32 yr. old regular guy who got interested in technology. About a year ago, I began dabbling with JAVA, followed by some PYTHON. It got to a point where I wasn't disciplined enough with my studies and needed some structure. That's where the GA Tech bootcamp came in. It offered me the structure that I needed to take the final step towards learning coding. I plan to finish the camp in February, and look for work in the field soon thereafter. Contact me thru Linkedin 👇")
     return(
         <div className="card text-center">
         <div className="card-header bg-primary text-white" >
