dfb9a805579fb35d534364a64300a3f8b0bb300c
rob49157 <robertopaipo@hotmail.com>
2022-03-03 21:27:23 -0500

    projectpage

--- root/src/components/Projects.js
+++ root/src/components/Projects.js
@@ -4,10 +4,17 @@ import React,{useState} from "react"
 import 'bootstrap/dist/css/bootstrap.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import { Table } from "react-bootstrap";
+import { Table, Card, Button, Link, Container } from "react-bootstrap";
 import crypto from '../images/cryptopicture.PNG'
 import bikeadventure from '../images/bikeadventure.png'
 import calendar from '../images/redcalendar.PNG'
+import note from '../images/notetakerpic.PNG'
+import password from '../images/passgenpic.PNG'
+import porfolio from '../images/porfolipic.PNG'
+import quiz from '../images/quizpic.PNG'
+import blog from '../images/techblogpic.PNG'
+import weather from '../images/weatherdashpic.PNG'
+import tech from '../images/techblogpic.PNG'
 import '../Styles/projectstyle.css'
 
  
@@ -16,72 +23,110 @@ function Projects() {
   return (
     <div style={{ display: 'block', width: 700, padding: 30 }}>
     <h4 style={{fontFamily:'serif',textAlign: 'center'}}>Projects</h4>
-    <Table className="projectstyle" >
-      <Col style={{
-        backgroundColor: 'rgb(207, 182, 182)',
-        fontFamily: 'serif'
-      }}><img className='textwrap-img' src={calendar} />
-        <a href="https://rob49157.github.io/work-day-scheduler/">🐦Interactive-calendar</a>
-        <p>First group project! This app allows the user to create an interactive calendar</p>
-    </Col>
-      <Col style={{
-        backgroundColor: 'rgb(207, 182, 182)',
-        fontFamily: 'serif'
-      }}><img className='textwrap-img' src={bikeadventure} />
-         <a href="https://CChampness.github.io/Bike-Adventure-Planning">  🐦Bike-adventure</a>
-        <p>First group project! The purpose of the app is to display bike trails throughout the planet.
-            New trails are being added on daily basis.</p>
-    </Col>
-      <Col style={{
-        backgroundColor: 'rgb(207, 182, 182)',
-        fontFamily: 'serif'
-      }}> <img className='textwrap-img' src={crypto} />
-         <a href="https://tranquil-everglades-59089.herokuapp.com/">  🐦crypto-dashboard</a>
-        <p>Second group project! The crypto--dashboard app. The app display live crypto prices and contains vast resources on crypto currencies</p>
-    </Col>
-    <Col style={{
-        backgroundColor: 'rgb(207, 182, 182)',
-        fontFamily: 'serif'
-      }}>
-        <a href="https://cryptic-cliffs-66103.herokuapp.com/login">  🐦Tech-Blog</a>
-        <p>The app allows the user to create a tech-related blog once Logged in into his/her account</p>
-    </Col>
-    <Col style={{
-        backgroundColor: 'rgb(207, 182, 182)',
-        fontFamily: 'serif'
-      }}>
-        <a href="https://tranquil-bastion-13264.herokuapp.com/">  🐦Note-Taker</a>
-        <p>this app connects the app note-taker from front end to the back end using GET and POST.</p>
-    </Col>
-    <Col style={{
-        backgroundColor: 'rgb(207, 182, 182)',
-        fontFamily: 'serif'
-      }}>
-      <a href="https://github.com/rob49157/weather-dashboard/commits/main">  🐦Weather-Dashboard</a>
-        <p>The weather dashboard allows you to view the weather of any city in the world. It also gives the user the ability to have their past city searches saved and have them display on the website.Uses API calls</p>
-    </Col>
-    <Col style={{
-        backgroundColor: 'rgb(207, 182, 182)',
-        fontFamily: 'serif'
-      }}>
-        <a href="https://rob49157.github.io/quiz/">  🐦Quiz</a>
-        <p>A quiz app with five questions that keeps tracks of scores and gives a final grade once completed. Data is store in local storage</p>
-    </Col>
-    <Col style={{
-        backgroundColor: 'rgb(207, 182, 182)',
-        fontFamily: 'serif'
-      }}>
-        <a href="https://rob49157.github.io/password-generator/">  🐦Password-Generator</a>
-        <p>This app creates random passowords for users</p>
-    </Col>
-       <Col style={{
-        backgroundColor: 'rgb(207, 182, 182)',
-        fontFamily: 'serif'
-      }}>
-        <a href="https://rob49157.github.io/portfolio">  🐦Portfolio page</a>
-        <p>First porfolio page</p>
-    </Col>
-    </Table>
+   <Container classname="cards">
+    <Card   style={{ width: '40rem' ,  }}>
+  <Card.Img variant="top" src={calendar} />
+  <Card.Body>
+    <Card.Title>Interactive-calendar</Card.Title>
+    <Card.Text> This app allows the user to create an interactive calendar
+     
+    </Card.Text>
+    <Card.Link href="https://rob49157.github.io/work-day-scheduler/">Interactive-calendar</Card.Link>
+    
+  </Card.Body>
+</Card>
+style
+<Card  style={{ width: '40rem' }}>
+  <Card.Img variant="top" src={bikeadventure} />
+  <Card.Body>
+    <Card.Title>Bike-Adventure</Card.Title>
+    <Card.Text>First group project! The purpose of the app is to display bike trails throughout the planet.
+            New trails are being added on daily basis
+     
+    </Card.Text>
+    <Card.Link href="https://CChampness.github.io/Bike-Adventure-Planning">Bike-adventure</Card.Link>
+    
+  </Card.Body>
+</Card>
+<Card   style={{ width: '40rem' }}>
+  <Card.Img variant="top" src={crypto} />
+  <Card.Body>
+    <Card.Title>Crypto-Dashboard</Card.Title>
+    <Card.Text>Second group project! The crypto--dashboard app. The app display live crypto prices and contains vast resources on crypto currencie
+     
+    </Card.Text>
+    <Card.Link href="https://tranquil-everglades-59089.herokuapp.com/">Crypto-Dashboard</Card.Link>
+    
+  </Card.Body>
+</Card>
+<Card  style={{ width: '40rem' }}>
+  <Card.Img variant="top" src={tech} />
+  <Card.Body>
+    <Card.Title>Tech-Blog</Card.Title>
+    <Card.Text>The app allows the user to create a tech-related blog once Logged in into his/her account
+     
+    </Card.Text>
+    <Card.Link href="https://cryptic-cliffs-66103.herokuapp.com/login">Tech-Blog</Card.Link>
+    
+  </Card.Body>
+</Card>
+<Card  style={{ width: '40rem' }}>
+  <Card.Img variant="top" src={note} />
+  <Card.Body>
+    <Card.Title>Note-Taker</Card.Title>
+    <Card.Text>this app connects the app note-taker from front end to the back end using GET and POST.
+     
+    </Card.Text>
+    <Card.Link href="https://tranquil-bastion-13264.herokuapp.com/">Note-Taker</Card.Link>
+    
+  </Card.Body>
+</Card>
+<Card  style={{ width: '40rem' }}>
+  <Card.Img variant="top" src={weather} />
+  <Card.Body>
+    <Card.Title>Weather-Dashboard</Card.Title>
+    <Card.Text>The weather dashboard allows you to view the weather of any city in the world. It also gives the user the ability to have their past city searches saved and have them display on the website.Uses API calls
+     
+    </Card.Text>
+    <Card.Link href="https://CChampness.github.io/Bike-Adventure-Planning">Weather-Dashboard</Card.Link>
+    
+  </Card.Body>
+</Card>
+<Card  style={{ width: '40rem' }}>
+  <Card.Img variant="top" src={quiz} />
+  <Card.Body>
+    <Card.Title>Quiz</Card.Title>
+    <Card.Text>A quiz app with five questions that keeps tracks of scores and gives a final grade once completed with a 32 second timer. Data is store in local storage
+     
+    </Card.Text>
+    <Card.Link href="https://rob49157.github.io/quiz/">Quiz</Card.Link>
+    
+  </Card.Body>
+</Card>
+<Card style={{ width: '40rem' }}>
+  <Card.Img variant="top" src={password} />
+  <Card.Body>
+    <Card.Title>Password-Generator</Card.Title>
+    <Card.Text>This app creates random passowords for users
+     
+    </Card.Text>
+    <Card.Link href="https://rob49157.github.io/password-generator/">Password-Generator</Card.Link>
+    
+  </Card.Body>
+</Card>
+<Card style={{ width: '40rem' }}>
+  <Card.Img variant="top" src={porfolio} />
+  <Card.Body>
+    <Card.Title>HTML,JS,CSS Porfolio Page</Card.Title>
+    <Card.Text>Portfolio page using only CSS for styling
+     
+    </Card.Text>
+    <Card.Link href="https://rob49157.github.io/portfolio">Original Portfolio</Card.Link>
+    
+  </Card.Body>
+</Card>
+</Container>
+   
   </div>
 );
 }
