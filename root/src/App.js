3c12badc9214833490c29c7ea22cd2ddf2ff55fd
rob49157 <robertopaipo@hotmail.com>
2022-04-07 19:16:01 -0400

    fsafsad

--- root/src/App.js
+++ root/src/App.js
@@ -8,7 +8,7 @@ import Home from './components/Home';
 
 import './Styles/Header.css'
 import { Navbar, Container, Nav, Modal, Tab } from "react-bootstrap";
-import { BrowserRouter as Router, Switch, Route, Routes, BrowserRouter } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Routes, BrowserRouter } from "react-router-dom";
 
 
 // const App= () => <Header/>
@@ -33,11 +33,11 @@ function App() {
 
   <Navbar className="navbar" bg="dark" variant="dark">
 
-    <Navbar.Brand Classname='metalgear' href="/react-portfolio/home" element={Home}>Roberto's page</Navbar.Brand>
+    <Navbar.Brand Classname='metalgear' href="/home" element={Home}>Roberto's page</Navbar.Brand>
     <Nav className="me-auto">
       {/* <Nav.Link href="/" element={Home}></Nav.Link> */}
-      <Nav.Link href="/react-portfolio/About" element={About}>About ME</Nav.Link>
-      <Nav.Link href="/react-portfolio/Projects" element={Projects}>Projects</Nav.Link>
+      <Nav.Link href="/About" element={About}>About ME</Nav.Link>
+      <Nav.Link href="/Projects" element={Projects}>Projects</Nav.Link>
       
       {/* <Nav.Link href="/Resume" element={Contact}>Resume</Nav.Link> */}
       <Nav.Link href="https://twitter.com/home" >Twitter</Nav.Link>
@@ -54,10 +54,10 @@ function App() {
  
   <Router>
     <Routes>
-      <Route exact path="/react-portfolio/home" element={ <Home />} />
-      <Route exact path="/react-portfolio/Projects" element={<Projects  />} />
-      <Route exact path="/react-portfolio/About" element={<About/>} />
-      <Route exact path="/react-portfolio/Contact" element={<Contact/>} />
+      <Route exact path="/home" element={ <Home />} />
+      <Route exact path="/Projects" element={<Projects  />} />
+      <Route exact path="/About" element={<About/>} />
+      <Route exact path="/Contact" element={<Contact/>} />
 
     </Routes>
   </Router>
