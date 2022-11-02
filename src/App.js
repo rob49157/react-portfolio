5b61414026ff6f2641539c27929c52e32e10ada0
rob49157 <robertopaipo@hotmail.com>
2022-11-01 21:27:36 -0400

    build fixes

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Projects from './components/Projects';
+import Footer from './components/Footer'
+import Contact from './components/Contact'
+import About from './components/About';
+import Home from './components/Home';
+
+import './Styles/Header.css'
+import { Navbar, Container, Nav, Modal, Tab } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Routes, BrowserRouter } from "react-router-dom";
+
+
+// const App= () => <Header/>
+
+
+// export default App;
+
+
+
+ 
+function App() {
+ return(
+
+ 
+  <div className="App">
+      
+
+
+
+
+
+
+  <Navbar className="navbar" bg="dark" variant="dark">
+
+    <Navbar.Brand Classname='metalgear' href="/react-portfolio/" element={Home}>Roberto's page</Navbar.Brand>
+    <Nav className="me-auto">
+      {/* <Nav.Link href="/" element={Home}></Nav.Link> */}
+      <Nav.Link href="/react-portfolio/About" element={About}>About </Nav.Link>
+      <Nav.Link href="/react-portfolio/Projects" element={Projects}>Projects</Nav.Link>
+      <Nav.Link href="/react-portfolio/Contact" element={Contact}>Contact</Nav.Link>
+      
+      {/* <Nav.Link href="/Resume" element={Contact}>Resume</Nav.Link> */}
+     
+      
+
+
+
+
+    </Nav>
+
+  </Navbar>
+ 
+  <BrowserRouter basename='/react-portfolio'>
+    <Routes >
+      <Route exact path="/" element={ <Home />} />
+      <Route exact path="/Projects" element={<Projects  />} />
+      <Route exact path="/About" element={<About/>} />
+      <Route exact path="/Contact" element={<Contact/>} />
+
+    </Routes>
+  </BrowserRouter>
+
+
+
+ 
+    
+
+
+              
+              
+  </div>
+ )
+  
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,69 @@
+// Import useEffect from React.
+import React, { useState, useEffect } from 'react';
+import '../Styles/Header.css'
+import Footer from './Footer';
+import { Container,Nav,Link,Navbar } from 'react-bootstrap';
+import { contains } from 'cheerio/lib/static';
+import Projects from '../components/Projects'
+import contact from '../components/Contact'
+import About from './About';
+{/* <a href='https://www.freepik.com/vectors/social'>Social vector created by myriammira - www.freepik.com</a> */}
+
+const styles = {
+  headerStyle: {
+    background: '#0071BC'
+  },
+  headingStyle: {
+    fontsize: '50px',
+  }
+}
+// href={`https://CChampness.github.io/Bike-Adventure-Planning`}
+// href={`#mailto:robertopaipo@hotmail.com`}
+
+const Header = (props) => {
+  const {
+    portfolioselected,setPortfolioSelected,
+    contactSelected,setContactSelected,
+    aboutSelected, setAboutselected,
+  } = props
+  return (
+    
+      <nav>
+        <header style={styles.headerStyle} className="header">
+          <h1 style={styles.headingStyle}>Roberto's React Portfolio</h1>
+          <div class="navbar-nav">
+            <a class="nav-item nav-link" ><span onClick={() => {setPortfolioSelected(true); setContactSelected(false); setAboutselected(false)}}>Projects</span></a>
+            <a class="nav-item nav-link" ><span onClick={() => {setContactSelected(true); setAboutselected(false); setPortfolioSelected(false)}}>Resume</span></a>
+            <a class="nav-item nav-link" ><span onClick={() => {setAboutselected(true); setContactSelected(false); setPortfolioSelected(false)}}>About Me</span></a>
+          </div>
+          <body>
+           
+          </body>
+
+
+
+
+
+
+        </header>
+      </nav>
+    
+    
+  );
+}
+
+<Navbar bg="primary" variant="dark">
+<Container>
+<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+<Nav className="me-auto">
+  <Nav.Link href="#home">Home</Nav.Link>
+  <Nav.Link href="#features">Features</Nav.Link>
+  <Nav.Link href="#pricing">Pricing</Nav.Link>
+</Nav>
+</Container>
+</Navbar>
+
+
+
+
+export default Header
